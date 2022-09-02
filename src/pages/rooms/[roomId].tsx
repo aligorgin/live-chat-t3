61a91b6bfd3d472da69baf2ce3951463fb1d8281
ali <aliwolfi18@gmail.com>
2022-09-02 21:38:28 +0430

    chat ui

--- src/pages/rooms/[roomId].tsx
+++ src/pages/rooms/[roomId].tsx
@@ -1,10 +1,34 @@
 import {useRouter} from "next/router";
 import {signIn, useSession} from "next-auth/react";
+import {useState} from "react";
+import {Message} from "../../constants/schemas";
+import {trpc} from "../../utils/trpc";
+import {Session} from "next-auth";
+
+
+function MessageItem({message, session}: { message: Message, session: Session }) {
+    return <li>
+        {message.message}
+    </li>
+}
 
 export default function RoomPage() {
     const {query} = useRouter();
     const roomId = query.roomId as string;
     const {data: session} = useSession();
+
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState<Message[]>([])
+
+    const {mutateAsync: sendMessageMutation} = trpc.useMutation(['room.send-message']);
+
+    trpc.useSubscription(['room.onSendMessage', {roomId}], {
+        onNext: (message) => {
+            setMessages((m) => {
+                return [...m, message]
+            })
+        }
+    })
 
     if (!session) {
         return (
@@ -16,7 +40,18 @@ export default function RoomPage() {
 
     return (
         <div>
-            welcome to room {roomId}
+            <ul>
+                {messages.map((m) => {
+                    return <MessageItem message={m} session={session}/>
+                })}
+            </ul>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                sendMessageMutation({roomId, message})
+            }}>
+                <textarea placeholder='what do you want to say'></textarea>
+                <button>Send message</button>
+            </form>
         </div>
     )
 }
