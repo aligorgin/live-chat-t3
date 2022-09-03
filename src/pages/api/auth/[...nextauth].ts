aef870d012767fff94a1c1263118c64a8b6bb8b3
ali <aliwolfi18@gmail.com>
2022-09-03 06:05:50 +0430

    reformat

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,37 +7,37 @@ import {prisma} from "../../../server/db/client";
 import {env} from "../../../env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
-    // Include user.id on session
-    // callbacks: {
-    //     session({session, user}) {
-    //         if (session.user) {
-    //             session.user.id = user.id;
-    //         }
-    //         return session;
-    //     },
-    // },
-    // Configure one or more authentication providers
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                name: {
-                    label: 'Name',
-                    type: 'text',
-                    placeholder: 'Enter your name',
-                }
-            },
-            async authorize(credentials, _req) {
-                const user = {id: 1, name: credentials?.name ?? "J Smith"};
-                return user;
+   // Include user.id on session
+   // callbacks: {
+   //     session({session, user}) {
+   //         if (session.user) {
+   //             session.user.id = user.id;
+   //         }
+   //         return session;
+   //     },
+   // },
+   // Configure one or more authentication providers
+   adapter: PrismaAdapter(prisma),
+   providers: [
+      CredentialsProvider({
+         name: 'Credentials',
+         credentials: {
+            name: {
+               label: 'Name',
+               type: 'text',
+               placeholder: 'Enter your name',
             }
-        })
-    ],
-    secret: env.NEXTAUTH_SECRET,
-    session: {
-        strategy: 'jwt',
-    }
+         },
+         async authorize(credentials, _req) {
+            const user = {id: 1, name: credentials?.name ?? "J Smith"};
+            return user;
+         }
+      })
+   ],
+   secret: env.NEXTAUTH_SECRET,
+   session: {
+      strategy: 'jwt',
+   }
 };
 
 export default NextAuth(authOptions);
--- src/pages/rooms/[roomId].tsx
+++ src/pages/rooms/[roomId].tsx
@@ -7,51 +7,50 @@ import {Session} from "next-auth";
 
 
 function MessageItem({message, session}: { message: Message, session: Session }) {
-    return <li>
-        {message.message}
-    </li>
+   return <li>
+      {message.message}
+   </li>
 }
 
 export default function RoomPage() {
-    const {query} = useRouter();
-    const roomId = query.roomId as string;
-    const {data: session} = useSession();
+   const {query} = useRouter();
+   const roomId = query.roomId as string;
+   const {data: session} = useSession();
 
-    const [message, setMessage] = useState('')
-    const [messages, setMessages] = useState<Message[]>([])
+   const [message, setMessage] = useState('')
+   const [messages, setMessages] = useState<Message[]>([])
 
-    const {mutateAsync: sendMessageMutation} = trpc.useMutation(['room.send-message']);
+   const {mutateAsync: sendMessageMutation} = trpc.useMutation(['room.send-message']);
 
-    trpc.useSubscription(['room.onSendMessage', {roomId}], {
-        onNext: (message) => {
-            setMessages((m) => {
-                return [...m, message]
-            })
-        }
-    })
+   trpc.useSubscription(['room.onSendMessage', {roomId}], {
+      onNext: (message) => {
+         setMessages((m) => {
+            return [...m, message]
+         })
+      }
+   })
 
-    if (!session) {
-        return (
-            <div>
-                <button onClick={() => signIn()}>Login</button>
-            </div>
-        )
-    }
+   if (!session) {
+      return (
+         <div>
+            <button onClick={() => signIn()}>Login</button>
+         </div>
+      )
+   }
 
-    return (
-        <div>
-            <ul>
-                {messages.map((m) => {
-                    return <MessageItem message={m} session={session}/>
-                })}
-            </ul>
-            <form onSubmit={(e) => {
-                e.preventDefault()
-                sendMessageMutation({roomId, message})
-            }}>
-                <textarea placeholder='what do you want to say'></textarea>
-                <button>Send message</button>
-            </form>
-        </div>
-    )
+   return (
+      <div>
+         <ul>
+            {messages.map((m) => {
+               return <MessageItem key={m.id} message={m} session={session}/>
+            })}
+         </ul>
+         <form onSubmit={(e) => {
+            sendMessageMutation({roomId, message})
+         }}>
+            <textarea placeholder='what do you want to say'></textarea>
+            <button>Send message</button>
+         </form>
+      </div>
+   )
 }
